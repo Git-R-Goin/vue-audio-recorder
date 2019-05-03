98f02ff8aba89ef5df2108b37296eba25d3c7000
KinDev <>
2019-05-04 02:58:50 +1000

    modify upload event

--- src/components/uploader.vue
+++ src/components/uploader.vue
@@ -1,5 +1,5 @@
 <style lang="scss">
-  @import '../scss/icons';
+@import "../scss/icons";
 </style>
 
 <template>
@@ -7,37 +7,37 @@
 </template>
 
 <script>
-  import IconButton from './icon-button'
-  import UploaderPropsMixin from '@/mixins/uploader-props'
+import IconButton from "./icon-button";
+import UploaderPropsMixin from "@/mixins/uploader-props";
 
-  export default {
-    mixins: [UploaderPropsMixin],
-    props: {
-      record: { type: Object }
-    },
-    components: {
-      IconButton
-    },
-    methods: {
-      upload () {
-        if (!this.record.url) {
-          return
-        }
-
-        this.$eventBus.$emit('start-upload')
-
-        const data = new FormData()
-        data.append('audio', this.record.blob, `${this.filename}.mp3`)
-
-        const headers = Object.assign(this.headers, {})
-        headers['Content-Type'] = `multipart/form-data; boundary=${data._boundary}`
-
-        this.$http.post(this.uploadUrl, data, { headers: headers }).then(resp => {
-          this.$eventBus.$emit('end-upload', { status: 'success', response: resp })
-        }).catch(error => {
-          this.$eventBus.$emit('end-upload', { status: 'fail', response: error })
-        })
+export default {
+  mixins: [UploaderPropsMixin],
+  props: {
+    record: { type: Object }
+  },
+  components: {
+    IconButton
+  },
+  methods: {
+    upload() {
+      if (!this.record.url) {
+        return;
       }
+
+      this.$eventBus.$emit("start-upload", this.record.blob);
+
+      // const data = new FormData()
+      // data.append('audio', this.record.blob, `${this.filename}.mp3`)
+
+      // const headers = Object.assign(this.headers, {})
+      // headers['Content-Type'] = `multipart/form-data; boundary=${data._boundary}`
+
+      // this.$http.post(this.uploadUrl, data, { headers: headers }).then(resp => {
+      //   this.$eventBus.$emit('end-upload', { status: 'success', response: resp })
+      // }).catch(error => {
+      //   this.$eventBus.$emit('end-upload', { status: 'fail', response: error })
+      // })
     }
   }
+};
 </script>
